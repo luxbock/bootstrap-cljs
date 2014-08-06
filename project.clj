3683f855b683644bf9c55190537e529d25074651
Paul English <paul@onfrst.com>
2014-08-06 09:39:52 -0600

    Ensure that cljx file gets included in deployed jar

--- project.clj
+++ project.clj
@@ -3,8 +3,9 @@
   :url "https://github.com/luxbock/bootstrap-cljs"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :jar-exclusions [#"\.cljx|\.swp|\.swo|\.DS_Store"]
-  :source-paths ["src"]
+  :jar-exclusions [#"\.cljx\.swp|\.swo|\.DS_Store"]
+  :source-paths ["target/generated/src/clj" "src"]
+  :resource-paths ["target/generated/src/cljs"]
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [prismatic/om-tools "0.2.2"]
                  [org.clojure/clojurescript "0.0-2268"]]
@@ -21,17 +22,19 @@
                    [lein-figwheel "0.1.3-SNAPSHOT"]]
          :source-paths ["src" "devcards_src"]}}
   :cljx {:builds [{:source-paths ["src"]
-                   :output-path  "target/generated/src"
+                   :output-path  "target/generated/src/clj"
                    :rules        :clj}
                   {:source-paths ["src"]
-                   :output-path  "target/generated/src"
+                   :output-path  "target/generated/src/cljs"
                    :rules        :cljs}]}
-  :cljsbuild {:builds [{:source-paths ["target/generated/src"]
+  :prep-tasks ["cljx" "javac" "compile"]
+
+  :cljsbuild {:builds [{:source-paths ["target/generated/src/cljs"]
                         :compiler {:output-to     "target/main.js"
                                    :optimizations :whitespace
                                    :pretty-print  true}}
                        {:id "devcards"
-                        :source-paths ["target/generated/src" "devcards_src"]
+                        :source-paths ["target/generated/src/cljs" "devcards_src"]
                         :compiler {:output-to     "resources/public/js/devcards.js"
                                    :output-dir    "resources/public/js/out"
                                    :source-map    "resources/public/js/devcards.js.map"
