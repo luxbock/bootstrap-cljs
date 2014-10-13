491083ba38be9bac40fc4ca157c22939099fed25
Olli Piepponen <opieppo@gmail.com>
2014-10-13 19:30:10 +0700

    Upgraded Devcards and Figwheel dependencies

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :jar-exclusions [#"\.cljx\.swp|\.swo|\.DS_Store"]
   :source-paths ["target/generated/src/clj" "src"]
-  :resource-paths ["target/generated/src/cljs"]
+  :resource-paths ["target/generated/src/cljs" "resources"]
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [prismatic/om-tools "0.2.2"]
                  [org.clojure/clojurescript "0.0-2268"]]
@@ -13,13 +13,13 @@
         :url "https://github.com/luxbock/bootstrap-cljs"}
   :deploy-repositories [["clojars" {:creds :gpg}]]
   :profiles
-  {:dev {:dependencies [[devcards "0.1.1-SNAPSHOT"]
+  {:dev {:dependencies [[devcards "0.1.2-SNAPSHOT"]
                         [weasel "0.3.0"]
                         [om "0.6.4"]]
          :plugins [[org.clojure/clojurescript "0.0-2268"]
                    [com.keminglabs/cljx "0.4.0"]
                    [lein-cljsbuild "1.0.3"]
-                   [lein-figwheel "0.1.3-SNAPSHOT"]]
+                   [lein-figwheel "0.1.4-SNAPSHOT"]]
          :source-paths ["src" "devcards_src"]}}
   :cljx {:builds [{:source-paths ["src"]
                    :output-path  "target/generated/src/clj"
