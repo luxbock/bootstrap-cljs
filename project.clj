4e3f6f96a4abdf94d0d84d77e4796b2d00cfed98
Greg Tebbutt <greg@sparrho.com>
2015-11-01 17:59:44 +0000

    Update libraries to latest versions

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject bootstrap-cljs "0.0.3"
+(defproject bootstrap-cljs "0.0.4-SNAPSHOT"
   :description "ClojureScript wrapper around React Bootstrap"
   :url "https://github.com/luxbock/bootstrap-cljs"
   :license {:name "Eclipse Public License"
@@ -6,36 +6,33 @@
   :jar-exclusions [#"\.cljx\.swp|\.swo|\.DS_Store"]
   :source-paths ["target/generated/src/clj" "src"]
   :resource-paths ["target/generated/src/cljs" "resources"]
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [prismatic/om-tools "0.2.2"]
-                 [org.clojure/clojurescript "0.0-2268"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/clojurescript "1.7.122"]
+                 [prismatic/om-tools "0.3.12"]]
   :scm {:name "git"
         :url "https://github.com/luxbock/bootstrap-cljs"}
   :deploy-repositories [["clojars" {:creds :gpg}]]
   :profiles
-  {:dev {:dependencies [[devcards "0.1.2-SNAPSHOT"]
-                        [weasel "0.3.0"]
-                        [om "0.6.4"]]
-         :plugins [[org.clojure/clojurescript "0.0-2268"]
-                   [com.keminglabs/cljx "0.4.0"]
-                   [lein-cljsbuild "1.0.3"]
-                   [lein-figwheel "0.1.4-SNAPSHOT"]]
+  {:dev {:dependencies [[devcards "0.2.0-8"]
+                        [weasel "0.7.0"]
+                        [org.omcljs/om "0.9.0"]
+                        [com.cemerick/piggieback "0.2.1"]
+                        [org.clojure/tools.nrepl "0.2.10"]]
+         :plugins [[lein-cljsbuild "1.1.0"]
+                   [lein-figwheel "0.4.1"]]
          :source-paths ["src" "devcards_src"]}}
-  :cljx {:builds [{:source-paths ["src"]
-                   :output-path  "target/generated/src/clj"
-                   :rules        :clj}
-                  {:source-paths ["src"]
-                   :output-path  "target/generated/src/cljs"
-                   :rules        :cljs}]}
-  :prep-tasks ["cljx" "javac" "compile"]
+  :prep-tasks ["javac" "compile"]
 
-  :cljsbuild {:builds [{:source-paths ["target/generated/src/cljs"]
+  :cljsbuild {:builds [{:source-paths ["src"]
                         :compiler {:output-to     "target/main.js"
                                    :optimizations :whitespace
                                    :pretty-print  true}}
                        {:id "devcards"
-                        :source-paths ["target/generated/src/cljs" "devcards_src"]
-                        :compiler {:output-to     "resources/public/js/devcards.js"
+                        :source-paths ["src" "devcards_src"]
+                        :figwheel { :devcards true }
+                        :compiler {:main "bootstrap-cljs-devcards"
+                                   :asset-path    "js/out"
+                                   :output-to     "resources/public/js/devcards.js"
                                    :output-dir    "resources/public/js/out"
                                    :source-map    "resources/public/js/devcards.js.map"
                                    :optimizations :none}}]})
